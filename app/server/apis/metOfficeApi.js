7872f3f728c0a33fa92f9f343391ae645f1950c8
Richard Lake <ralake1985@gmail.com>
2018-10-21 19:43:46 +0100

    Flesh out graphql responses

--- app/server/apis/metOfficeApi.js
+++ app/server/apis/metOfficeApi.js
@@ -36,17 +36,19 @@ const WEATHER_TYPES = [
 
 module.exports = {
   async getForecastForLocation (locationId) {
-    const totalHours = 24
     const hourly = 3
     const apiKey = process.env.MET_OFFICE_API_KEY
     const { data } = await axios.get(`http://datapoint.metoffice.gov.uk/public/data/val/wxfcs/all/json/${locationId}?res=${hourly}hourly&key=${apiKey}`)
-    const { Location: location } = data.SiteRep.DV
 
-    return formatDays(location, totalHours, hourly)
+    if (!data) return
+
+    const { Location: location } = data.SiteRep.DV
+    return formatDays(location, hourly)
   }
 }
 
-function formatDays (location, totalHours, hourly) {
+function formatDays (location, hourly) {
+  const totalHours = 24
   const { Period: days } = location
   const maxRanges = totalHours / hourly
   const rangeStartTimes = getStartTimes(totalHours, hourly)
@@ -54,18 +56,18 @@ function formatDays (location, totalHours, hourly) {
   return days.reduce(
     (memo, day) => {
       const ranges = day.Rep.length
-      const date = new Date(day.value)
+      const date = new Date(day.value).toISOString()
       let rangeIndex = maxRanges - ranges
 
-      memo.forecast.days.push({
-        dateTs: date.getTime(),
+      memo.dailyForecast.push({
+        date: date,
         timeRanges: day.Rep.map(range => {
           const rangeData = {
             precipitationProbability: Number(range.Pp),
             feelsLikeTemperature: Number(range.F),
             temperature: Number(range.T),
             overview: WEATHER_TYPES[Number(range.W)],
-            ts: new Date(day.value).setHours(rangeStartTimes[rangeIndex])
+            startsAt: new Date(new Date(day.value).setHours(rangeStartTimes[rangeIndex])).toISOString()
           }
 
           rangeIndex++
@@ -80,9 +82,7 @@ function formatDays (location, totalHours, hourly) {
       name: location.name,
       country: location.country,
       continent: location.continent,
-      forecast: {
-        days: []
-      }
+      dailyForecast: []
     }
   )
 }
